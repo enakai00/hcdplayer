fb1e16cb8059cf893bc411c71828fe0e41911682
Etsuji Nakai <enakai@redhat.com>
2013-09-18 08:06:00 +0900

    Converted to F19/GHC7.4

--- hcdplayer.hs
+++ hcdplayer.hs
@@ -1,11 +1,13 @@
 -- hcdplay.hs
 --
 -- ver1.0 2013/09/17 Tested with Fedora17/GHC7.0.4 
+-- ver2.0 2013/09/18 Tested with Fedora19/GHC7.4.2
 --
 -- usage: hcdplay <directory of music files>
 --
 
-import System
+import System.Environment
+import System.Exit
 import System.IO
 import System.Directory (doesDirectoryExist, getDirectoryContents)
 import System.FilePath ((</>))
@@ -13,18 +15,15 @@ import System.Cmd (system)
 import System.Console.Haskeline 
 import Text.Printf (printf)
 import Text.Regex.Posix ((=~))
-import Data.List (sort)
 import Data.Maybe
-import List (nub)
+import Data.List (sort, nub)
 import Control.Monad
-import Codec.Binary.UTF8.String
+import Control.Exception as Exception
 
 type Albums = (String, [String]) -- (Artist, [Album])
 type Dirpath = String
 
 -- Misc functions --
-
-puts = putStrLn . Codec.Binary.UTF8.String.decodeString
 
 split :: String -> String -> (String,String)
 split pat str = fstAndThird (str =~ pat :: (String,String,String))
@@ -51,11 +50,19 @@ selectMenu ask range = runInputT defaultSettings $ loop ask range
                     if range num then return num
                                  else loop ask range -- Tail Recursion
 
+usage :: IO()
+usage = do
+    progName <- getProgName
+    putStrLn $ "Usage: " ++ progName ++ " <music file directory>"
+    exitWith ExitSuccess
+
 main :: IO ()
 main = do
     hSetBuffering stdin  NoBuffering
     hSetBuffering stdout NoBuffering
-    topDir <- fmap head getArgs
+    args <- getArgs
+    when (length args /= 1) $ usage
+    let topDir = head args
     albums <- selectArtist topDir
     when (albums == Nothing) $ exitWith ExitSuccess
     albumDir <- selectAlbum topDir $ fromJust albums
@@ -69,9 +76,9 @@ selectArtist topDir = do
     let artists = filter ((/=)"") $
                   sort $ nub $ map (fst.split " - ") dirs 
     system "clear"
-    puts "Welcome to cdplayer.\n"
-    mapM_ puts $ addNumber artists
-    puts ""
+    putStrLn "Welcome to cdplayer.\n"
+    mapM_ putStrLn $ addNumber artists
+    putStrLn ""
     num <- selectMenu "Select artist (q:quit)? "
                       (\x -> x > 0 && x <= length artists)
     let artist = artists !! (num - 1)
@@ -83,9 +90,9 @@ selectArtist topDir = do
 selectAlbum :: Dirpath -> Albums -> IO (Maybe Dirpath)
 selectAlbum topDir (artist, albums) = do 
     system "clear"
-    puts $ "Albums of " ++ artist ++ "\n"
-    mapM_ puts $ addNumber $ albums
-    puts ""
+    putStrLn $ "Albums of " ++ artist ++ "\n"
+    mapM_ putStrLn $ addNumber $ albums
+    putStrLn ""
     num <- selectMenu "Select album (q:back)? "
                       (\x -> x > 0 && x <= length albums)
     let albumDir = topDir </> artist ++ " - " ++ (albums !! (num - 1))
@@ -97,3 +104,4 @@ playSongs albumDir = do
     system "clear"
     system $ "cd \"" ++ albumDir ++ "\";mplayer * 2>/dev/null" 
     return ()
+
